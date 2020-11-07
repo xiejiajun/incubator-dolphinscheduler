796acafffac723bc5ea7444361d319a3e3bcc438
xiejiajun <xiejiajun02@163.con>
2020-11-07 12:31:16 +0800

    Dag 新增依赖时死循环检测 & 整个DAG通过拓扑排序进行环状结构检测源码分析

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/graph/DAG.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/graph/DAG.java
@@ -515,5 +515,108 @@ public class DAG<Node, NodeInfo, EdgeInfo> {
 
   }
 
+
+//  /**
+//   * 判断增加边(fromVertex --> toVertex)是否合法,需要判断是否符合无环的约束
+//   *
+//   * @param fromVertex     起点
+//   * @param toVertex       终点
+//   * @param createVertex 是否创建结点
+//   * @return
+//   */
+//  private boolean isLegalAddEdge(Vertex fromVertex, Vertex toVertex, boolean isCreate) {
+//    if (fromVertex.equals(toVertex)) {
+//      logger.error("edge fromVertex({}) can't equals toVertex({})",fromVertex,toVertex);
+//      return false;
+//    }
+//    if (!isCreate) {
+//      if (!hasVertex(fromVertex) || !hasVertex(toVertex)){
+//        logger.error("edge fromVertex({}) or toVertex({}) is not in vertices map",fromVertex,toVertex);
+//        return false;
+//      }
+//    }
+//    Queue<Vertex> queue = new LinkedList<>();
+//    queue.add(toVertex);
+//    int verticesCount = getVerticesCount();
+//    //如果没有找到fromVertex, 表示无环;
+//    while (!queue.isEmpty() && verticesCount > 0) {
+//      verticesCount -= 1;
+//      Vertex key = queue.poll();
+//      // 遍历后续顶点
+//      for (Vertex subsequentVertex : getSubsequentNodes(key)) {
+//        if (subsequentVertex.equals(fromVertex)) {
+//          return false;
+//        }
+//        queue.add(subsequentVertex);
+//      }
+//    }
+//    return true;
+//  }
+//
+//  /**
+//   * 判断是否有环及拓扑排序结果
+//   *
+//   * 有向无环图(DAG)才有拓扑(topological)排序
+//   * 广度优先遍历的主要做法：
+//   *    1、遍历图中所有的顶点，将入度为0的顶点入队列。
+//   *    2、从队列中poll出一个顶点，更新该顶点的邻接点的入度(减1)，如果邻接点的入度减1之后等于0，则将该邻接点入队列。
+//   *    3、一直执行第2步，直到队列为空。
+//   * 如果无法遍历完所有的结点，则意味着当前的图不是有向无环图。不存在拓扑排序。
+//   *
+//   *
+//   * @return key返回的是状态, 如果成功(无环)为true, 失败则有环， value为拓扑排序结果(可能是其中一种)
+//   */
+//  private Map.Entry<Boolean, List<Vertex>> topologicalSortImpl() {
+//    //入度为0的结点队列
+//    Queue<Vertex> zeroIndegreeVertexQueue = new LinkedList<>();
+//    //保存结果
+//    List<Vertex> topoResultList = new ArrayList<>();
+//    //保存入度不为0的结点
+//    Map<Vertex, Integer> notZeroIndegreeVertexMap = new HashMap<>();
+//
+//    //扫描所有的顶点,将入度为0的顶点入队列
+//    for (Map.Entry<Vertex, VertexInfo> vertices : verticesMap.entrySet()) {
+//      Vertex vertex = vertices.getKey();
+//      int inDegree = getIndegree(vertex);
+//
+//      if (inDegree == 0) {
+//        zeroIndegreeVertexQueue.add(vertex);
+//        topoResultList.add(vertex);
+//      } else {
+//        notZeroIndegreeVertexMap.put(vertex, inDegree);
+//      }
+//    }
+//
+//    //扫描完后，没有入度为0的结点，说明有环，直接返回
+//    if(zeroIndegreeVertexQueue.isEmpty()){
+//      return new AbstractMap.SimpleEntry(false, topoResultList);
+//    }
+//
+//    //采用topology算法, 删除入度为0的结点和它的关联边
+//    while (!zeroIndegreeVertexQueue.isEmpty()) {
+//      Vertex v = zeroIndegreeVertexQueue.poll();
+//      //得到相邻结点
+//      Set<Vertex> subsequentNodes = getSubsequentNodes(v);
+//
+//      for (Vertex subsequentVertex : subsequentNodes) {
+//
+//        Integer degree = notZeroIndegreeVertexMap.get(subsequentVertex);
+//
+//        if(--degree == 0){
+//          topoResultList.add(subsequentVertex);
+//          zeroIndegreeVertexQueue.add(subsequentVertex);
+//          notZeroIndegreeVertexMap.remove(subsequentVertex);
+//        }else{
+//          notZeroIndegreeVertexMap.put(subsequentVertex, degree);
+//        }
+//
+//      }
+//    }
+//
+//    //notZeroIndegreeVertexMap如果为空, 表示没有环
+//    AbstractMap.SimpleEntry resultMap = new AbstractMap.SimpleEntry(notZeroIndegreeVertexMap.size() == 0 , topoResultList);
+//    return resultMap;
+//
+//  }
 }
 
